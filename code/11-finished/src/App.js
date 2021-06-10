1a0217bfa0a2bf839845e4c9e2c4a1c94b501dd9
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-10 22:06:59 +0700

    add NewExpense to App

--- code/11-finished/src/App.js
+++ code/11-finished/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-import Expenses from './components/Expenses/Expenses';
+import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
 
 const App = () => {
   const expenses = [
@@ -8,22 +9,22 @@ const App = () => {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 7, 14)
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 2, 28)
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+      date: new Date(2021, 5, 12)
+    }
+  ]
 
   // return React.createElement(
   //   'div',
@@ -34,10 +35,10 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
